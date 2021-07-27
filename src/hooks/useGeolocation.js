58798169803b5cbf48e080fe782385a899606e12
Pau Larrea <paularrea12@hotmail.com>
2021-07-27 12:51:30 +0200

    get geolocation try

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export const useGeolocation = () => {
+  const [location, setLocation] = useState(null);
+  const [userIP, setUserIP] = useState(null);
+  const [city, setCity] = useState(null);
+  const [region, setRegion] = useState(null);
+
+  // Only run on mount and unmount
+  useEffect(() => {
+    const setValues = json => {
+      setLocation(json);
+      setUserIP(json.ip);
+      setCity(json.city);
+      setRegion(json.region);
+    };
+
+    fetch("https://ipapi.co/json")
+      .then(data => data.json())
+      .then(json => {
+        setValues(json);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return {
+    location,
+    setLocation,
+    userIP,
+    city,
+    region
+  };
+};
+
+
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,8 +5,9 @@ import MediaQuery from "react-responsive";
 import "../styles/App.scss";
 import Navbar from "../components/Navigation/navbar.jsx";
 import { UserContext } from "../UserContext.js";
-// import useCurrentLocation from "../hooks/useCurrentLocation.js";
+
 import LayoutDesktop from "../components/LayoutDesktop/LayoutDesktop.jsx";
+import { useGeolocation } from "../hooks/useGeolocation.js";
 
 // const geolocationOptions = {
 //   // Using this option you can define when should the location request timeout and
@@ -16,34 +17,42 @@ import LayoutDesktop from "../components/LayoutDesktop/LayoutDesktop.jsx";
 
 const Dashboard = ({ Logout }) => {
   const { contextUser } = useContext(UserContext);
-  // const { location, error } = useCurrentLocation(geolocationOptions);
+  const { location, userIP, city, region } = useGeolocation;
+  // const [coordinates, setCoordinates] = useState({});
 
-
-  // const userAgent = navigator.userAgent;
-
-  // console.log(location, error, 'location')
-
-
+  // const getCoordinates = () => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     setCoordinates({
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude,
+  //     });
+  //   });
+  // };
+  const getLocation = () => {
+    console.log(location && location, "location");
+  };
   return (
     <>
-        <div className={styles.container}>
-          <Navbar user={contextUser} Logout={Logout} />
-          <div className={styles.flex_desktop}>
-            <LayoutDesktop />
-            <section>
-              <MediaQuery minWidth={1026}>
-                <h3 style={{ marginBottom: "1rem" }}>Próximas citas</h3>
-                <div className="container-mobile">
-                  <ProximasCitas user={contextUser} />
-                </div>
-              </MediaQuery>
-            </section>
-          </div>
-
-          <MediaQuery maxWidth={1025}>
-            <ProximasCitas user={contextUser} />
-          </MediaQuery>
+      <div className={styles.container}>
+        <Navbar user={contextUser} Logout={Logout} />
+        <div className={styles.flex_desktop}>
+          <LayoutDesktop />
+          {/* <button onClick={getCoordinates}>Show my location</button> */}
+          <section>
+            <MediaQuery minWidth={1026}>
+              <h3 style={{ marginBottom: "1rem" }}>Próximas citas</h3>
+              <div className="container-mobile">
+                <ProximasCitas user={contextUser} />
+              </div>
+            </MediaQuery>
+          </section>
+          <button onClick={getLocation}>Location</button>
         </div>
+
+        <MediaQuery maxWidth={1025}>
+          <ProximasCitas user={contextUser} />
+        </MediaQuery>
+      </div>
     </>
   );
 };
